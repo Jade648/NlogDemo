33c1f8cb7a0ec1f85a98400de6b90c71df4b567d
Jade645 <maddiepeterson375@gmail.com>
2022-09-08 09:27:39 -0500

    Logger

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NlogDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var config = new NLog.Config.LoggingConfiguration();
+
+// Targets where to log to: File and Console
+var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "file.txt" };
+var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+            
+// Rules for mapping loggers to targets            
+config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+            
+// Apply config           
+NLog.LogManager.Configuration = config;
+        }
+    }
+}
+
